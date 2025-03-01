c19f2183ed4d99606c2e4f7a3a209887e945efb2
정필성 <ibetter.kr@gmail.com>
2025-03-01 17:00:27 +0900

    코드 수정

--- src/main/java/com/example/spring/posts/PostsController.java
+++ src/main/java/com/example/spring/posts/PostsController.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -17,41 +17,42 @@ public class PostsController {
     PostsService postsService;
 
     // 게시글 등록 (화면, GET)
-    @GetMapping("/posts/create")
+    @RequestMapping(value = "/posts/create", method = RequestMethod.GET)
     public String createGet() {
         return "posts/create";
     }
 
     // 게시글 등록 (처리, POST)
-    @PostMapping("/posts/create")
+    @RequestMapping(value = "/posts/create", method = RequestMethod.POST)
     public String createPost(PostsVo postsVo, RedirectAttributes redirectAttributes) {
         boolean created = postsService.create(postsVo);
+
         if (created) {
             redirectAttributes.addFlashAttribute("successMessage", "게시글이 등록되었습니다.");
             return "redirect:/posts/";
-        } else {
-            redirectAttributes.addFlashAttribute("errorMessage", "게시글 등록에 실패했습니다.");
-            return "redirect:/posts/create";
         }
+
+        redirectAttributes.addFlashAttribute("errorMessage", "게시글 등록에 실패했습니다.");
+        return "redirect:/posts/create";
     }
 
-    // 게시글 목록
-    @GetMapping("/posts/")
+    // 게시글 목록 (화면, GET)
+    @RequestMapping(value = "/posts/", method = RequestMethod.GET)
     public String listGet(Model model) {
         List<PostsVo> postsVoList = postsService.list();
         model.addAttribute("postsVoList", postsVoList);
         return "posts/list";
     }
 
-    // 게시글 보기
-    @GetMapping("/posts/{id}")
+    // 게시글 보기 (화면, GET)
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
     public String readGet(@PathVariable("id") int id, Model model) {
         model.addAttribute("postsVo", postsService.read(id));
         return "posts/read";
     }
 
     // 게시글 수정 (화면, GET)
-    @GetMapping("/posts/{id}/update")
+    @RequestMapping(value = "/posts/{id}/update", method = RequestMethod.GET)
     public String updateGet(@PathVariable("id") int id, Model model) {
         model.addAttribute("postsVo", postsService.read(id));
         return "posts/update";
